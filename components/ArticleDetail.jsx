afaf6f5b610d921a994a152f22a5bc28a6b3ad35
bluesky2006 <simon.busby@gmail.com>
2025-06-24 17:08:49 +0100

    Rename: ArticleFull to ArticleDetail

---
+++ components/ArticleDetail.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { fetchArticleById } from "../src/api";
+import { useParams } from "react-router-dom";
+import { convertDate } from "../utils";
+
+function ArticleDetail() {
+  const [article, setArticle] = useState(null);
+  const [loading, setLoading] = useState("");
+  const [error, setError] = useState(null);
+
+  let { article_id } = useParams();
+
+  useEffect(() => {
+    setLoading("Loading");
+    setError(null);
+    setArticle(null);
+
+    fetchArticleById(article_id)
+      .then(({ article }) => {
+        setArticle(article);
+        setLoading("");
+        console.log(article, "<< article in ArticleDetail");
+      })
+      .catch((err) => {
+        console.log(err, "<< error in ArticleDetail>>");
+        setError(err.msg);
+        setLoading("");
+      });
+  }, [article_id]);
+
+  if (error) {
+    return (
+      <section>
+        <p>No article</p>
+      </section>
+    );
+  }
+
+  if (!article) {
+    return (
+      <section>
+        <p>{loading}</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <div>
+        <h3 id="full-article-title">{article.title}</h3>
+        <div className="article-header">
+          <img
+            src={article.article_img_url}
+            alt="Article image"
+            className="article-image"
+          />
+          <p>{article.body}</p>
+        </div>
+        <div>
+          <div className="article-metadata">
+            <p className="pill">Author: {article.author}</p>
+            <p className="pill">Topic: {article.topic}</p>
+            <p className="pill">Date: {convertDate(article.created_at)}</p>
+            <p className="pill">Votes: {article.votes}</p>
+            <p className="pill">Comments: {article.comment_count}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ArticleDetail;
