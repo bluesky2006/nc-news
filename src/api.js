be3f804d2b4fd2a8bdbb44f82fb08c80e0cb18ec
bluesky2006 <simon.busby@gmail.com>
2025-06-24 16:05:32 +0100

    Add: api fetching to two endpoints

---
+++ src/api.js
@@ -0,0 +1,31 @@
+export function fetchArticles(startIndex, endIndex) {
+  return fetch("https://nc-news-3jz4.onrender.com/api/articles")
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject({
+          status: res.status,
+          msg: "Failed to fetch articles",
+        });
+      }
+      return res.json();
+    })
+    .then((articles) => {
+      return articles.articles.slice(startIndex, endIndex);
+    });
+}
+
+export function fetchArticleById(article_id) {
+  return fetch(`https://nc-news-3jz4.onrender.com/api/articles/${article_id}`)
+    .then((res) => {
+      if (!res.ok) {
+        return Promise.reject({
+          status: res.status,
+          msg: "Failed to fetch article",
+        });
+      }
+      return res.json();
+    })
+    .then((article) => {
+      return article;
+    });
+}
