8c9bf15995b72545ab1e15acc8bc08f879289296
bluesky2006 <simon.busby@gmail.com>
2025-06-25 16:45:00 +0100

    Update: to remove optimistic render of comment and to push new comment to comments array via setComments

--- components/CommentForm.jsx
+++ components/CommentForm.jsx
@@ -1,47 +1,31 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
 import { postCommentById } from "../src/api";
-import Comment from "./Comment";
 
-function CommentForm() {
+function CommentForm({ article_id, setComments, comments }) {
   const [input, setInput] = useState("");
-  const [comment, setComment] = useState(null);
   const [posting, setPosting] = useState(false);
   const [error, setError] = useState(false);
-
-  let { article_id } = useParams();
+  const [user] = useState("grumpy19");
 
   function handleInputChange(event) {
     setInput(event.target.value);
   }
-
-  // 3. function to handle comment submission
 
   function handleSubmit(event) {
     event.preventDefault();
     setPosting(true);
     setError(false);
 
-    setComment({
-      comment: {
-        body: input,
-        votes: 0,
-        author: "grumpy19",
-        created_at: new Date().toISOString(),
-        comment_id: Date.now(),
-      },
-    });
-
     const body = {
-      username: "grumpy19",
+      username: user,
       body: input,
     };
 
     postCommentById(article_id, body)
-      .then((comment) => {
-        setComment(comment);
-        setPosting(false);
+      .then((response) => {
+        setComments([response.comment, ...comments]);
         setInput("");
+        setPosting(false);
       })
       .catch(() => {
         setError(true);
@@ -50,25 +34,21 @@ function CommentForm() {
   }
 
   return (
-    <section>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="add-comment">New comment</label>
-          <div>
-            <input
-              id="add-comment"
-              type="text"
-              value={input}
-              onChange={handleInputChange}
-            />
-            <button type="submit">Post</button>
-          </div>
-        </form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="add-comment">New comment</label>
+      <div className="input-and-button-div">
+        <input
+          id="add-comment"
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+        />
+        <button type="submit" disabled={posting}>
+          {posting ? "Posting..." : "Post"}
+        </button>
       </div>
-      {posting && <p>Posting...</p>}
       {error && <p>Failed to post comment</p>}
-      {comment && <Comment key={comment.comment_id} comments={[comment]} />}
-    </section>
+    </form>
   );
 }
 
