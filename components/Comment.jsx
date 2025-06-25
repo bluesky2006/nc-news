be7dbeeaad72d08d77ad0e1be1177700fd2ac3ac
bluesky2006 <simon.busby@gmail.com>
2025-06-25 23:28:45 +0100

    Add: delete comment function

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,14 +1,28 @@
 import { convertDate } from "../utils";
+import { deleteCommentById } from "../src/api";
+import { useState } from "react";
 
-function Comment({ comments }) {
+function Comment({ comments, setComments }) {
+  const [deleted, setDeleted] = useState(null);
   if (!comments) {
     return null;
   }
+
+  function handleDelete(comment_id) {
+    deleteCommentById(comment_id).then(() => {
+      setDeleted(`Your comment (${comment_id}) was deleted.`);
+      setComments((currComments) =>
+        currComments.filter((comment) => comment.comment_id !== comment_id)
+      );
+    });
+  }
+
   return (
     <section>
+      {deleted && <p className="delete-message">{deleted}</p>}
       {comments.map((comment, index) => {
         return (
-          <section key={index} className="comment">
+          <section key={index} className="card comment">
             <p className="comment-body">{comment.body}</p>
             <div className="metadata">
               <p className="pill">
@@ -20,6 +34,11 @@ function Comment({ comments }) {
               <p className="pill">
                 <strong>Votes:</strong> {comment.votes}
               </p>
+            </div>
+            <div className="delete-div">
+              <button onClick={() => handleDelete(comment.comment_id)}>
+                Delete comment
+              </button>
             </div>
           </section>
         );
