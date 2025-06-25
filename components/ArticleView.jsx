226351bda0fb29a3c5c92dd1480760b81195ff6f
bluesky2006 <simon.busby@gmail.com>
2025-06-25 16:12:51 +0100

    Delete: CommentForm element

--- components/ArticleView.jsx
+++ components/ArticleView.jsx
@@ -1,14 +1,12 @@
 // This component is for combining ArticleDetail (by id) + associated Comments + Comment posting
 
 import ArticleDetail from "./ArticleDetail";
-import CommentForm from "./CommentForm";
 import CommentsList from "./CommentsList";
 
 function ArticleView() {
   return (
     <section>
       <ArticleDetail />
-      <CommentForm />
       <CommentsList />
     </section>
   );
