3b821c27d303595cf616143b431e107ab5568636
bluesky2006 <simon.busby@gmail.com>
2025-06-26 12:45:57 +0100

    Update: site structure to support new HomePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
-import ArticlesList from "../components/ArticlesList";
-import ArticleDetail from "../components/ArticleDetail";
 import ArticleView from "../components/ArticleView";
+import HomePage from "../components/HomePage";
 
 function App() {
   return (
     <main>
       <Header />
       <Routes>
-        <Route path="/" element={<ArticlesList />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/article/:article_id" element={<ArticleView />} />
       </Routes>
     </main>
