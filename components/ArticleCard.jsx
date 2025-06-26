92a9fad8f6b6a2dfc182a197ca7e7e12a2a022cf
bluesky2006 <simon.busby@gmail.com>
2025-06-26 14:54:05 +0100

    Change: styling for pill alignment

--- components/ArticleCard.jsx
+++ components/ArticleCard.jsx
@@ -4,29 +4,31 @@ import { convertDate } from "../utils";
 function ArticleCard({ article }) {
   return (
     <div key={article.article_id} className="card">
-      <Link to={`/article/${article.article_id}`}>
-        <div className="article-card-header">
-          <img
-            src={article.article_img_url}
-            alt="Article image"
-            className="article-card-image"
-          />
-          <h3>{article.title}</h3>
+      <div className="article-card-header">
+        <img
+          src={article.article_img_url}
+          alt="Article image"
+          className="article-card-image"
+        />
+        <div className="article-card-content">
+          <Link to={`/articles/${article.article_id}`}>
+            <h3>{article.title}</h3>
+          </Link>
+          <div className="metadata">
+            <p className="pill">
+              <strong>Author:</strong> {article.author}
+            </p>
+            <p className="pill">
+              <strong>Topic:</strong> {article.topic}
+            </p>
+            <p className="pill">
+              <strong>Date:</strong> {convertDate(article.created_at)}
+            </p>
+            <p className="pill">
+              <strong>Votes:</strong> {article.votes}
+            </p>
+          </div>
         </div>
-      </Link>
-      <div className="metadata">
-        <p className="pill">
-          <strong>Author:</strong> {article.author}
-        </p>
-        <p className="pill">
-          <strong>Topic:</strong> {article.topic}
-        </p>
-        <p className="pill">
-          <strong>Date:</strong> {convertDate(article.created_at)}
-        </p>
-        <p className="pill">
-          <strong>Votes:</strong> {article.votes}
-        </p>
       </div>
     </div>
   );
