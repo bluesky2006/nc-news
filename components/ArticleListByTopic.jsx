6e1bee62920a0301e117c86377ec279b3eb3dfcb
bluesky2006 <simon.busby@gmail.com>
2025-06-26 14:52:05 +0100

    Add: ArticleListByTopic component

---
+++ components/ArticleListByTopic.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { fetchArticlesByQuery } from "../src/api";
+import { useParams } from "react-router-dom";
+import ArticleCard from "./ArticleCard";
+
+function ArticleListByTopic() {
+  const { topic } = useParams();
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  console.log(topic, "asahjfds");
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    setArticles(null);
+
+    fetchArticlesByQuery(topic)
+      .then((articles) => {
+        setArticles(articles);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, [topic]);
+
+  if (loading) {
+    return <p>Loading articles...</p>;
+  }
+
+  if (error) {
+    return <p>Failed to load articles</p>;
+  }
+
+  if (!articles) return null;
+
+  return (
+    <section>
+      <h2>
+        {topic.replace(/\b\w/g, (firstLetter) => firstLetter.toUpperCase())}{" "}
+        articles
+      </h2>
+      <div>
+        {articles.map((article) => {
+          return <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default ArticleListByTopic;
