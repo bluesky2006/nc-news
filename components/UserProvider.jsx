2844256d83761976647023344fabcd801f6928cb
bluesky2006 <simon.busby@gmail.com>
2025-06-26 14:50:02 +0100

    Change: user choice

--- components/UserProvider.jsx
+++ components/UserProvider.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { UserContext } from "./UserContext";
 
 export const UserProvider = ({ children }) => {
-  const [loggedInUser, setLoggedInUser] = useState({ name: "jessjelly" });
+  const [loggedInUser, setLoggedInUser] = useState({ name: "tickle122" });
 
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
