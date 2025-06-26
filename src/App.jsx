d18c47b98943b2855c5b7547824da2180d6a0593
bluesky2006 <simon.busby@gmail.com>
2025-06-26 16:55:58 +0100

    Add: FilterBar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import ArticleView from "../components/ArticleView";
-import HomePage from "../components/HomePage";
+import ArticlesList from "../components/ArticlesList";
 import ArticleListByTopic from "../components/ArticleListByTopic";
+import FilterBar from "../components/FilterBar";
 
 function App() {
   return (
     <main>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/articles/:article_id" element={<ArticleView />} />
-        <Route path="/topics/:topic" element={<ArticleListByTopic />} />
-      </Routes>
+      <FilterBar variant="topbar" />
+      <div className="content-layout">
+        <FilterBar variant="sidebar" />{" "}
+        <Routes>
+          <Route path="/" element={<ArticlesList />} />{" "}
+          <Route path="/articles/:article_id" element={<ArticleView />} />
+          <Route path="/topics/:topic" element={<ArticleListByTopic />} />
+        </Routes>
+      </div>
     </main>
   );
 }
