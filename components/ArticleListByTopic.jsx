58b1c6a3fe14490bde34d4249489ac4499cf8988
bluesky2006 <simon.busby@gmail.com>
2025-06-27 13:57:52 +0100

    Update: error handling to show the FilterBar if a user tries to look at a topic that doesn't exist

--- components/ArticleListByTopic.jsx
+++ components/ArticleListByTopic.jsx
@@ -1,36 +1,48 @@
 import { useEffect, useState } from "react";
 import { fetchArticlesByTopic } from "../src/api";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import ArticleCard from "./ArticleCard";
+import FilterBar from "./FilterBar";
 
 function ArticleListByTopic() {
   const { topic } = useParams();
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [searchParams] = useSearchParams();
+
+  const sortBy = searchParams.get("sort_by") || "created_at";
+  const order = searchParams.get("order") || "desc";
 
   useEffect(() => {
     setLoading(true);
     setError("");
     setArticles(null);
 
-    fetchArticlesByTopic(topic)
+    fetchArticlesByTopic(topic, sortBy, order)
       .then((articles) => {
         setArticles(articles);
         setLoading(false);
       })
       .catch((err) => {
-        setError(err.msg);
+        setError(err.status);
         setLoading(false);
       });
-  }, [topic]);
+  }, [topic, sortBy, order]);
 
   return loading ? (
     <p>Loading articles...</p>
-  ) : error ? (
-    <p>{error}</p>
+  ) : error === 404 ? (
+    <section>
+      <FilterBar articleListByTopicError={error} />
+      <p>
+        That topic does not exist – try picking an existing one from the
+        selection above.
+      </p>
+    </section>
   ) : !articles ? null : (
     <section>
+      <FilterBar />
       <h2>
         {topic.replace(/\b\w/g, (letter) => letter.toUpperCase())} articles
       </h2>
