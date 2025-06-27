c8cab8afd23c9633afeb768d151ea2016439b7c9
bluesky2006 <simon.busby@gmail.com>
2025-06-27 14:30:23 +0100

    Add: simple utility functon for api requests

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export function convertDate(oldDate) {
   const date = new Date(oldDate);
   const formatted = date.toLocaleString("en-GB", {
@@ -9,3 +11,29 @@ export function convertDate(oldDate) {
   });
   return formatted;
 }
+
+const useApiRequest = (apiFunction, param) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    setError("");
+
+    apiFunction(param)
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        setError(err.msg);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [apiFunction, param]);
+
+  return { data, setData, loading, error };
+};
+
+export default useApiRequest;
