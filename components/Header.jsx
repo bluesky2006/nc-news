38b9ca5026cabf2bc8dba4cc391adfe3eace454a
bluesky2006 <simon.busby@gmail.com>
2025-06-27 16:20:29 +0100

    Delete: logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
-import logo from "../src/assets/logo.svg";
 
 function Header() {
   return (
     <header>
       <Link to="/">
         <div className="header-div">
-          <img className="logo" src={logo} alt="Northcoder News logo" />
           <h1>northcoder news</h1>
         </div>
       </Link>
