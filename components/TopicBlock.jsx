8200588b8085ea9cdf3430e7f0376be776cdade0
bluesky2006 <simon.busby@gmail.com>
2025-07-23 11:07:08 +0100

    Update topic filtering component to highlight the button corresponding to the topic page being viewed

--- components/TopicBlock.jsx
+++ components/TopicBlock.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function TopicBlock({ topics }) {
+  const fullPath = useLocation();
+  const pathParts = fullPath.pathname.split("/");
+  const path = pathParts[pathParts.length - 1];
+
   return (
     <div className={"topic-section"}>
       <p>Browse articles by topic</p>
@@ -9,7 +14,12 @@ function TopicBlock({ topics }) {
           const { slug } = topic;
           return (
             <Link to={`/topics/${slug}`} key={slug}>
-              <button className="topic-button-outline">
+              <button
+                className={`topic-button-outline${
+                  slug === path ? " active" : ""
+                }`}
+              >
+                {" "}
                 {slug.replace(/\b\w/g, (letter) => letter.toUpperCase())}
               </button>
             </Link>
